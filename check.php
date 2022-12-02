9f4a8b638bafa39bf7aa85f585116f24b5122107
decorix <polevg2015@yandex.ru>
2022-12-02 19:18:42 +0300

    second

--- check.php
+++ check.php
@@ -1,4 +1,9 @@
 <?php
  $name = $_POST['name'];
-echo $name;
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+
+$mysql = new mysqli('localhost', 'root', 'root', 'web');
+$mysql->query("INSERT INTO 'user' ('name', 'email', 'password') VALUES ('$name', '$email', '$password')");
+$mysql->close();
 ?>
