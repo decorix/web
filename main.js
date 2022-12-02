09cc452405e3639805702ddfad4e87438ccfec9d
decorix <polevg2015@yandex.ru>
2022-12-02 18:40:14 +0300

    first

---
+++ main.js
@@ -0,0 +1,55 @@
+function Slideshow(element) {
+    this.el = document.querySelector(element);
+    this.init();
+}
+
+Slideshow.prototype = {
+    init: function() {
+        this.wrapper = this.el.querySelector(".slider-wrapper");
+        this.slides = this.el.querySelectorAll(".slide");
+        this.previous = this.el.querySelector(".slider-previous");
+        this.next = this.el.querySelector(".slider-next");
+        this.index = 0;
+        this.total = this.slides.length;
+
+        this.actions();
+    },
+    _slideTo: function(pointer) {
+        var currentSlide = this.slides[pointer];
+        this.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
+    },
+    actions: function() {
+        var self = this;
+        self.next.addEventListener(
+            "click",
+            function() {
+                self.index++;
+                self.previous.style.display = "block";
+
+                if (self.index == self.total - 1) {
+                    self.index = self.total - 1;
+                    self.next.style.display = "none";
+                }
+
+                self._slideTo(self.index);
+            },
+            false
+        );
+
+        self.previous.addEventListener(
+            "click",
+            function() {
+                self.index--;
+                self.next.style.display = "block";
+
+                if (self.index == 0) {
+                    self.index = 0;
+                    self.previous.style.display = "none";
+                }
+
+                self._slideTo(self.index);
+            },
+            false
+        );
+    },
+};
